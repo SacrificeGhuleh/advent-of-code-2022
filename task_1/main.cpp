23e66c3fce2119b4ca39cdeeae185f0d6ea516ae
SacrificeGhuleh <zvonek.r@seznam.cz>
2023-06-15 13:51:29 +0200

    Finished first task

--- task_1/main.cpp
+++ task_1/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <cstdint>
 #include <fstream>
+#include <set>
+
+struct Elf
+{
+    Elf(uint32_t calories, size_t idx) : calories{calories}, idx{idx} {}
+
+    bool operator<(const Elf &elf) const { return calories > elf.calories; }
+
+    uint32_t calories = 0;
+    size_t idx = 0;
+};
 
 int main()
 {
@@ -13,25 +24,43 @@ int main()
     // Use a while loop together with the getline() function to read the file line by line
     uint32_t accumulation = 0;
     uint32_t max_calories = 0;
+    size_t idx = 0;
+
+    std::set<Elf> elfs;
+
     while (std::getline(inputFile, input))
     {
         // Output the text from the file
 
         try
         {
-            // std::cout << std::stoi(input) << "\n";
             accumulation += std::stoi(input);
         }
         catch (std::exception const &ex)
         {
+            elfs.insert(Elf(accumulation, idx));
             max_calories = std::max(max_calories, accumulation);
             accumulation = 0;
+            ++idx;
         }
     }
+
+    elfs.insert(Elf(accumulation, idx));
     max_calories = std::max(max_calories, accumulation);
     accumulation = 0;
+    ++idx;
+
+    size_t count = 0;
+    uint32_t accumulationOfTopThree = 0;
+    for (auto const &elf : elfs)
+    {
+        if (++count > 3)
+            break;
+        accumulationOfTopThree += elf.calories;
+    }
 
     std::cout << "Max calories: " << max_calories << "\n";
+    std::cout << "Sum of top three: " << accumulationOfTopThree << "\n";
 
     // Close the file
     inputFile.close();
