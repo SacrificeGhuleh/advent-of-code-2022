886b604a712e8f9bc77528ad4069cc0f83697d19
SacrificeGhuleh <zvonek.r@seznam.cz>
2023-06-16 12:56:41 +0200

    FInished second task

--- task_2/main.cpp
+++ task_2/main.cpp
@@ -5,15 +5,42 @@
 
 constexpr uint8_t baseScoreTable[3] = {1, 2, 3};
 
+// A - Rock
+// B - Paper
+// C - Scissors
+
 constexpr uint8_t scoreTable[3][3] = {
-    {3, 6, 0},
-    {0, 3, 6},
-    {6, 0, 3},
+    /*     A  B  C*/
+    /*A*/ {3, 6, 0},
+    /*B*/ {0, 3, 6},
+    /*C*/ {6, 0, 3},
 };
+
+// X - Lose
+// Y - Draw
+// Z - Win
+constexpr char moveTable[3][3] = {
+    /*     X     Y    Z*/
+    /*A*/ {'C', 'A', 'B'},
+    /*B*/ {'A', 'B', 'C'},
+    /*C*/ {'B', 'C', 'A'},
+};
+
+constexpr std::pair<char, char> getWinningStrategy(std::pair<char, char> input)
+{
+    const char opponent = input.first;
+    const char result = input.second;
+    const size_t resultIdx = result - 'X';
+    const size_t opponentIdx = opponent - 'A';
+
+    const char player = moveTable[opponentIdx][resultIdx];
+
+    return {opponent, player};
+}
 
 constexpr uint8_t getScore(char opponent, char player)
 {
-    const size_t playerIdx = player - 'X';
+    const size_t playerIdx = player - 'A';
     const size_t opponentIdx = opponent - 'A';
 
     const uint8_t baseScore = baseScoreTable[playerIdx];
@@ -34,16 +61,24 @@ constexpr std::pair<char, char> parseInput(const char *line)
 
 int main()
 {
-    static_assert(getScore('A', 'Y') == 8);
-    static_assert(getScore('B', 'X') == 1);
-    static_assert(getScore('C', 'Z') == 6);
-
     static_assert(parseInput("A Y").first == 'A');
     static_assert(parseInput("A Y").second == 'Y');
 
-    static_assert(getScore(parseInput("A Y")) == 8);
-    static_assert(getScore(parseInput("B X")) == 1);
-    static_assert(getScore(parseInput("C Z")) == 6);
+    static_assert(getWinningStrategy(parseInput("A Y")).second == 'A');
+    static_assert(getWinningStrategy(parseInput("B Y")).second == 'B');
+    static_assert(getWinningStrategy(parseInput("C Y")).second == 'C');
+
+    static_assert(getWinningStrategy(parseInput("A X")).second == 'C');
+    static_assert(getWinningStrategy(parseInput("B X")).second == 'A');
+    static_assert(getWinningStrategy(parseInput("C X")).second == 'B');
+
+    static_assert(getWinningStrategy(parseInput("A Z")).second == 'B');
+    static_assert(getWinningStrategy(parseInput("B Z")).second == 'C');
+    static_assert(getWinningStrategy(parseInput("C Z")).second == 'A');
+
+    static_assert(getScore(getWinningStrategy(parseInput("A Y"))) == 4);
+    static_assert(getScore(getWinningStrategy(parseInput("B X"))) == 1);
+    static_assert(getScore(getWinningStrategy(parseInput("C Z"))) == 7);
 
     // Create a text string, which is used to output the text file
     std::string input;
@@ -55,7 +90,7 @@ int main()
 
     while (std::getline(inputFile, input))
     {
-        score += getScore(parseInput(input.c_str()));
+        score += getScore(getWinningStrategy(parseInput(input.c_str())));
     }
 
     std::cout << "Score: " << score << "\n";
