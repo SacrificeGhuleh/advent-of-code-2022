99f1fc23f78242d7fa4532ef620bce754cd4e107
SacrificeGhuleh <zvonek.r@seznam.cz>
2023-06-20 15:25:43 +0200

    Template update

--- template/main.cpp
+++ template/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <cstring>
+#include <cassert>
+#include <unordered_set>
+#include <fstream>
+#include <cstdint>
+#include <array>
+#include <vector>
 
-int main(){
-    std::cout << "Hello world\n";
+void test()
+{
+    assert(0 == 1);
+}
+
+int main()
+{
+    test();
+
+    // Create a text string, which is used to output the text file
+    std::string input;
+
+    // Read from the text file
+    std::ifstream inputFile("data/input.txt");
+
+    while (std::getline(inputFile, input))
+    {
+        // imput
+    }
+    // Close the file
+    inputFile.close();
 }
